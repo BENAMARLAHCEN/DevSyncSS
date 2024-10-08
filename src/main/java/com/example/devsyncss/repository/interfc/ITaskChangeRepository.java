c79da7fa9ca750a0ee6294e4cb38d624a61f597f
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-10-08 16:48:52 +0100

    Refactor ITaskChangeRepository and TaskChangeRequestService

--- src/main/java/com/example/devsyncss/repository/interfc/ITaskChangeRepository.java
+++ src/main/java/com/example/devsyncss/repository/interfc/ITaskChangeRepository.java
@@ -1,4 +1,14 @@
 package com.example.devsyncss.repository.interfc;
 
-public class ITaskChangeRepository {
+import com.example.devsyncss.entities.TaskChange;
+
+import java.util.List;
+
+public interface ITaskChangeRepository {
+    void createTaskChange(TaskChange taskChange);
+    TaskChange getTaskChange(Long id);
+    void updateTaskChange(TaskChange taskChange);
+    void deleteTaskChange(Long id);
+    List<TaskChange> getAllManagerCreateTaskChanges(Long managerId);
+
 }
--- src/main/java/com/example/devsyncss/service/TaskChangeRequestService.java
+++ src/main/java/com/example/devsyncss/service/TaskChangeRequestService.java
@@ -1,4 +1,16 @@
 package com.example.devsyncss.service;
 
-public class TaskChangeRequestService {
+import com.example.devsyncss.repository.TaskChangeRepository;
+import com.example.devsyncss.repository.interfc.ITaskChangeRepository;
+import com.example.devsyncss.service.interfc.ITaskChangeRequestService;
+
+public class TaskChangeRequestService implements ITaskChangeRequestService {
+    private ITaskChangeRepository taskChangeRepository;
+
+    public TaskChangeRequestService() {
+        this.taskChangeRepository = new TaskChangeRepository();
+    }
+
+
+
 }
